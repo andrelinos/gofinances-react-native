7e9c10c780721dab52dcc5beebe438dcce7f6fbc
Andrelino Silva <andrelinocom@gmail.com>
2021-09-21 20:23:28 -0300

    Add: typescript + styled-components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export function Dashbard() {
+    return (
+        <Container>
+            <Title>Dashbard</Title>
+        </Container>
+    );
+}
