14e5ef75be66f7f9a3c48cbbed2b2674fe732b90
Andrelino Silva <andrelinocom@gmail.com>
2021-09-23 07:53:09 -0300

    Add: Estilizando os tipos de cards

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,11 +1,15 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
-import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
-import theme from '../../global/styles/theme';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled.View`
+type CardTypeProps = {
+    type: 'up' | 'down' | 'total';
+};
+
+export const Container = styled.View<CardTypeProps>`
+    background-color: ${({ theme, type }) =>
+        type === 'total' ? theme.colors.secondary : theme.colors.shape};
     width: ${RFValue(300)}px;
-    background-color: ${({ theme }) => theme.colors.shape};
     border-radius: ${RFValue(5)}px;
     padding: ${RFValue(19)}px ${RFValue(23)}px;
     padding-bottom: ${RFValue(42)}px;
@@ -18,31 +22,52 @@ export const Header = styled.View`
     justify-content: space-between;
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<CardTypeProps>`
     font-family: ${({ theme }) => theme.fonts.medium};
     font-size: ${RFValue(14)}px;
 
-    color: ${({ theme }) => theme.colors.text_dark};
+    color: ${({ theme, type }) =>
+        type === 'total' ? theme.colors.shape : theme.colors.text_dark};
 `;
 
-export const Icon = styled(Feather)`
+export const Icon = styled(Feather)<CardTypeProps>`
     font-size: ${RFValue(40)}px;
+
+    ${({ type }) =>
+        type === 'up' &&
+        css`
+            color: ${({ theme }) => theme.colors.success};
+        `}
+
+    ${({ type }) =>
+        type === 'down' &&
+        css`
+            color: ${({ theme }) => theme.colors.attention};
+        `}
+    
+    ${({ type }) =>
+        type === 'total' &&
+        css`
+            color: ${({ theme }) => theme.colors.shape};
+        `}
 `;
 
 export const Footer = styled.View``;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<CardTypeProps>`
     font-family: ${({ theme }) => theme.fonts.medium};
     font-size: ${RFValue(32)}px;
 
-    color: ${({ theme }) => theme.colors.text_dark};
+    color: ${({ theme, type }) =>
+        type === 'total' ? theme.colors.shape : theme.colors.text_dark};
 
     margin-top: ${RFValue(32)}px;
 `;
 
-export const LastTransaction = styled.Text`
+export const LastTransaction = styled.Text<CardTypeProps>`
     font-family: ${({ theme }) => theme.fonts.medium};
     font-size: ${RFValue(12)}px;
 
-    color: ${({ theme }) => theme.colors.text};
+    color: ${({ theme, type }) =>
+        type === 'total' ? theme.colors.shape : theme.colors.text};
 `;
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -10,9 +10,24 @@ export function Dashbard() {
         <Container>
             <Header />
             <HighlightCards>
-                <HighlightCard />
-                <HighlightCard />
-                <HighlightCard />
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="17.400,00"
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="1.259,00"
+                    lastTransaction="Última saída dia 14 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="16.141,00"
+                    lastTransaction="01 a 16 de abril"
+                />
             </HighlightCards>
         </Container>
     );
