2877729b38b377ac351bab83699f9a11f48d5fcc
Andrelino Silva <andrelinocom@gmail.com>
2022-01-24 08:10:15 -0300

    Add: Input Test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     preset: 'jest-expo',
     testPathIgnorePatterns: ['/node_modules', '/android', '/ios'],
-    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+    setupFilesAfterEnv: [
+        '@testing-library/jest-native/extend-expect',
+        'jest-styled-components'
+    ],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     transformIgnorePatterns: [
         'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -1,17 +1,28 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { TextInput } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export const Container = styled(TextInput)`
-  width: 100%;
-  padding: ${RFValue(16)}px ${RFValue(18)}px;
-  
-  font-family: ${({ theme }) => theme.fonts.regular};
-  font-size: ${RFValue(14)}px;
+import { InputProps } from '.';
 
-  color: ${({ theme }) => theme.colors.text_dark};
-  background-color: ${({ theme }) => theme.colors.shape};
-  border-radius: 5px;
+export const Container = styled(TextInput)<InputProps>`
+    width: 100%;
+    padding: ${RFValue(16)}px ${RFValue(18)}px;
 
-  margin-bottom: ${RFValue(8)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size: ${RFValue(14)}px;
+
+    color: ${({ theme }) => theme.colors.text_dark};
+    background-color: ${({ theme }) => theme.colors.shape};
+    border-radius: 5px;
+
+    margin-bottom: ${RFValue(8)}px;
+
+    border-width: 3px;
+    border-color: transparent;
+
+    ${({ active, theme }) =>
+        active &&
+        css`
+            border-color: ${theme.colors.attention};
+        `}
 `;
