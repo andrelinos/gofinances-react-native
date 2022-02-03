1a91bb2dcaed1273b0b6abb38467853f273c06b7
Andrelino Silva <andrelinocom@gmail.com>
2022-02-03 08:13:42 -0300

    Resolvido: Update jest e resolutions

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,94 +1,74 @@
-import { jest } from '@jest/globals';
+import fetchMock from 'jest-fetch-mock';
+fetchMock.enableMocks();
+
 import { renderHook, act, RenderResult } from '@testing-library/react-hooks';
 import * as AuthSession from 'expo-auth-session';
 
 import { AuthProvider, useAuth } from './auth';
 
+const userTest = {
+    id: 'any_id',
+    email: 'john.doe@email.com',
+    name: 'John Doe',
+    photo: 'any_photo.png'
+};
+
 jest.mock('expo-auth-session');
 
-// jest.mock('@react-native-async-storage/async-storage', () => ({
-//     getItem: jest.fn(),
-//     setItem: jest.fn()
-// }));
-
-let hookResult: RenderResult<ReturnType<typeof useAuth>>;
-let googleMocked: any;
+jest.mock('@react-native-async-storage/async-storage', () => ({
+    getItem: jest.fn(),
+    setItem: jest.fn()
+}));
 
 describe('Auth Hook', () => {
-    beforeEach(() => {
-        const { result } = renderHook(() => useAuth(), {
-            wrapper: AuthProvider
-        });
-        hookResult = result;
-        googleMocked = jest.mock(AuthSession.startAsync as any);
-    });
-
     it('should be able to sign in with Google account existing', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify(userTest));
+        const googleMocked = jest.mocked(AuthSession.startAsync as any);
         googleMocked.mockReturnValueOnce({
             type: 'success',
-            accessToken: 'accessToken',
-            idToken: 'idToken',
-            user: {
-                id: 'userId',
-                email: 'john.doe@email.com',
-                name: 'John',
-                photo: 'any_photo.png'
+            params: {
+                accessToken: 'accessToken'
             }
         });
 
-        // await act(() => hookResult.current.signInWithGoogle());
+        const { result } = renderHook(() => useAuth(), {
+            wrapper: AuthProvider
+        });
 
-        // expect(hookResult.current.user.email).toBe('john.doe@email.com');
+        await act(() => result.current.signInWithGoogle());
+
+        expect(result.current.user.email).toBe('john.doe@email.com');
     });
-    // it('should be able to sign in with Google account existing', async () => {
-    //     fetchMock.mockResponseOnce(JSON.stringify(userTest));
-    //     const mockedGoogle = jest.mocked(AuthSession.startAsync as any);
 
-    //     console.log('MOCKED GOOGLE: => ', [mockedGoogle]);
+    it('user should not connect if cancel authentication with Google', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify(userTest));
+        const googleMocked = jest.mocked(AuthSession.startAsync);
 
-    //     mockedGoogle.mockReturnValueOnce({
-    //         type: 'success',
-    //         params: {
-    //             access_token: 'any_token'
-    //         }
-    //     });
+        googleMocked.mockReturnValue({
+            type: 'cancel',
+            params: {
+                access_token: ''
+            }
+        } as any);
 
-    //     const { result } = renderHook(() => useAuth(), {
-    //         wrapper: AuthProvider
-    //     });
+        const { result } = renderHook(() => useAuth(), {
+            wrapper: AuthProvider
+        });
 
-    //     await act(() => result.current.signInWithGoogle());
+        await act(() => result.current.signInWithGoogle());
 
-    //     console.table([result.current.user]);
+        expect(result.current.user).not.toHaveProperty('id');
+    });
 
-    //     expect(result.current.user.email).toBe('john.doe@email.com');
-    // });
+    it('should be error with incorrectly Google parameters', async () => {
+        const { result } = renderHook(() => useAuth(), {
+            wrapper: AuthProvider
+        });
 
-    // it('user should not connect if cancel authentication with Google', async () => {
-    //     googleMocked.mockReturnValueOnce({
-    //         type: 'canceled',
-    //         accessToken: ''
-    //     });
-
-    //     await act(() => hookResult.current.signInWithGoogle());
-
-    //     console.table([hookResult.current.user]);
-
-    //     expect(hookResult.current.user).not.toHaveProperty('id');
-    // });
-
-    // it('should be error with incorrectly Google parameters', async () => {
-    //     const { result } = renderHook(() => useAuth(), {
-    //         wrapper: AuthProvider
-    //     });
-
-    //     try {
-    //         await act(() => result.current.signInWithGoogle());
-
-    //         console.log('CORRECT: => ', result.current.user);
-    //     } catch {
-    //         expect(result.current.user).toEqual({});
-    //         console.log('INCORRECT: => ', result.current.user);
-    //     }
-    // });
+        try {
+            await act(() => result.current.signInWithGoogle());
+        } catch {
+            expect(result.current.user).toEqual({});
+        }
+    });
 });
--- src/screens/Register/register.spec.tsx
+++ src/screens/Register/register.spec.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { Register } from '.';
 import { ThemeProvider } from 'styled-components/native';
 import theme from '../../global/styles/theme';
 
-import { Register } from '.';
+jest.mock('@react-navigation/native', () => {
+    return {
+        useNavigation: jest.fn()
+    };
+});
 
-const Providers: React.FC = ({ children }) => (
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
-);
+const Providers: React.FC = ({ children }) => {
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
 
 describe('Register Screen', () => {
-    // it('should be open category modal when user click on button', () => {
-    //     const { getByTestId } = render(<Register />, {
-    //         wrapper: Providers
-    //     });
-    //     const categoryModal = getByTestId('modal-category');
-    //     const buttonCategory = getByTestId('button-category');
-    //     // expect(categoryModal.props.visible).toBeFalsy();
-    //     // expect(categoryModal.props.visible).toBeFalsy();
-    // });
+    it('should be open category modal when user click button', async () => {
+        const { getByTestId } = render(<Register />, {
+            wrapper: Providers
+        });
+
+        const categoryModal = getByTestId('modal-category');
+        const buttonCategory = getByTestId('button-category');
+        fireEvent.press(buttonCategory);
+
+        waitFor(() => {
+            expect(categoryModal.props.visible).toBeTruthy();
+        });
+    });
 });
